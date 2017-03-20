006a5845fee1a9504e8a4728f18b91453a66cc1c
Liah Wallace <liahwallace1@gmail.com>
2017-03-20 22:17:44 +0000

    working model methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,15 @@
 class Cart < ActiveRecord::Base
   belongs_to :user
-  has_many :line_items
+  has_many :line_items, :foreign_key => "cart_id"
   has_many :items, through: :line_items
 
   def add_item
   end
 
-  def self.total
+  def total
+    self.line_items.map do |line_item|
+      line_item.quantity * line_item.item.price
+    end.reduce(0, :+)
   end
 
 end
